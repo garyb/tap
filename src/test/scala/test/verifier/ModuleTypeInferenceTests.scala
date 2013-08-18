29fa5f5128b0f34d9de937fc80f1367afacd394e
Gary Burgess <garyb@server.fake>
2013-08-18 10:53:44 +0100

    Test makeInstanceMemberType

--- src/test/scala/test/verifier/ModuleTypeInferenceTests.scala
+++ src/test/scala/test/verifier/ModuleTypeInferenceTests.scala
@@ -2,9 +2,42 @@ package test.verifier
 
 import org.scalatest.{GivenWhenThen, FlatSpec}
 import org.scalatest.matchers.ShouldMatchers._
+import tap.types.classes._
+import tap.ModuleId
+import tap.types.kinds._
+import tap.types._
+import tap.types.Type._
+import language.reflectiveCalls
+import tap.types.classes.ClassEnvironments._
+import tap.verifier.ModuleTypeInference
 
 class ModuleTypeInferenceTests extends FlatSpec {
 
-    ignore should "have some tests" in {}
+    behavior of "apply"
+    ignore should "do some things" in {}
+
+    // ------------------------------------------------------------------------
+
+    behavior of "makeInstanceMemberType"
+
+    it should "instantiate a type for a particular class instance" in {
+        val sc = Qual(List(IsIn(ModuleId("Prelude", "Show"), List(TVar("a", Star)))), TVar("a", Star) fn tString)
+        val tci = Inst("Test", Nil, IsIn(ModuleId("Prelude", "Show"), List(tNumber)))
+        val mti = new ModuleTypeInference(Nil, Map.empty, Map.empty)
+        mti.makeInstanceMemberType(sc, tci) should be ===
+                Qual(Nil, tNumber fn tString)
+    }
+
+    ignore should "instantiate a type for a particular class instance when the class has multiple parameters" in {}
+
+    // ------------------------------------------------------------------------
+
+    behavior of "buildClassEnv"
+    ignore should "do some things" in {}
+
+    // ------------------------------------------------------------------------
+
+    behavior of "resolveBindingGroups"
+    ignore should "do some things" in {}
 
 }
