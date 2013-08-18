400d5fdcc2fa4a11c4bdeecabb8ec2e5cae75012
Gary Burgess <garyb@server.fake>
2013-08-18 13:12:34 +0100

    Testing resolveBindingGroups

--- src/test/scala/test/verifier/ModuleTypeInferenceTests.scala
+++ src/test/scala/test/verifier/ModuleTypeInferenceTests.scala
@@ -3,13 +3,14 @@ package test.verifier
 import org.scalatest.{GivenWhenThen, FlatSpec}
 import org.scalatest.matchers.ShouldMatchers._
 import tap.types.classes._
-import tap.ModuleId
+import tap._
 import tap.types.kinds._
 import tap.types._
 import tap.types.Type._
 import language.reflectiveCalls
 import tap.types.classes.ClassEnvironments._
 import tap.verifier.ModuleTypeInference
+import tap.ir._
 
 class ModuleTypeInferenceTests extends FlatSpec with GivenWhenThen {
 
@@ -80,6 +81,59 @@ class ModuleTypeInferenceTests extends FlatSpec with GivenWhenThen {
     // ------------------------------------------------------------------------
 
     behavior of "resolveBindingGroups"
-    ignore should "do some things" in {}
+
+    it should "group implicit definitions as tightly as possible" in {
+
+        /*
+            Equivalent to:
+
+            (def tmpX (-> a a))
+            (let tmpX (lambda (x) (tmpD x) x))
+
+            (let tmpA (lambda (a) (tmpB a) a))
+            (let tmpB (lambda (b) (tmpA b) (tmpX b) b))
+
+            (let tmpC (lambda (c) (tmpA c) (tmpD c) c))
+            (let tmpD (lambda (d) (tmpC d) d))
+
+            Or as a .dot graph:
+
+            digraph {
+              X [shape=box]
+              A -> B
+              B -> A
+              B -> X
+              C -> A
+              C -> D
+              D -> C
+              X -> D
+            }
+        */
+
+        val mis: Map[Id, TapExpr] = Map(
+            ModuleId("Prelude","tmpC") -> FunctionExpr(Argument("c"),BlockExpr(List(ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpA")),ValueReadExpr(LocalId("c"))), ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpD")),ValueReadExpr(LocalId("c"))), ValueReadExpr(LocalId("c"))))),
+            ModuleId("Prelude","tmpX") -> FunctionExpr(Argument("x"),BlockExpr(List(ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpD")),ValueReadExpr(LocalId("x"))), ValueReadExpr(LocalId("x"))))),
+            ModuleId("Prelude","tmpB") -> FunctionExpr(Argument("b"),BlockExpr(List(ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpA")),ValueReadExpr(LocalId("b"))), ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpX")),ValueReadExpr(LocalId("b"))), ValueReadExpr(LocalId("b"))))),
+            ModuleId("Prelude","tmpD") -> FunctionExpr(Argument("d"),BlockExpr(List(ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpC")),ValueReadExpr(LocalId("d"))), ValueReadExpr(LocalId("d"))))),
+            ModuleId("Prelude","tmpA") -> FunctionExpr(Argument("a"),BlockExpr(List(ApplyExpr(ValueReadExpr(ModuleId("Prelude","tmpB")),ValueReadExpr(LocalId("a"))), ValueReadExpr(LocalId("a")))))
+        )
+
+        val mts: Map[Id, Qual[Type]] = Map(
+            ModuleId("Prelude","tmpX") -> Qual(List(),Forall(6,List(Star),TAp(TAp(TCon(ModuleId("Prelude","->"),Kfun(Star,Kfun(Star,Star))),TGen(6,0)),TGen(6,0))))
+        )
+
+        val mti = new ModuleTypeInference(Nil, Map.empty, Map.empty)
+        val bgs = mti.resolveBindingGroups(mis, mts)
+
+        val expl = List(ModuleId("Prelude","tmpX"))
+        val impls = List(
+            List(ModuleId("Prelude","tmpB"), ModuleId("Prelude","tmpA")),
+            List(ModuleId("Prelude","tmpD"), ModuleId("Prelude","tmpC"))
+        )
+
+        bgs should be === List(expl, impls)
+    }
+
+    ignore should "do some other things" in {}
 
 }
