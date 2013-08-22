940e4040ff1e9bc97b6cecad47586b89d5abd952
Gary Burgess <garyb@server.fake>
2013-08-22 23:35:11 +0100

    Types still need kinds

--- src/main/scala/tap/types/Type.scala
+++ src/main/scala/tap/types/Type.scala
@@ -14,11 +14,11 @@ case class Forall(tvs: List[Tyvar], t: Type) extends Type
 case class TAp(f: Type, a: Type) extends Type
 case class TCon(id: ModuleId, k: Kind) extends Type
 case class TVar(tv: Tyvar) extends Type { def id: String = tv.id }
-case class MetaTv(i: Int, var ref: Some[Type]) extends Type
+case class MetaTv(i: Int, k: Kind, var ref: Option[Type]) extends Type
 
 sealed trait Tyvar { def id: String }
-case class BoundTv(id: String) extends Tyvar
-case class SkolemTv(id: String, i: Int) extends Tyvar
+case class BoundTv(id: String, k: Kind) extends Tyvar
+case class SkolemTv(id: String, i: Int, k: Kind) extends Tyvar
 
 object Type {
 
